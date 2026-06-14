4b824efdcbbd3ddb3098bc700edca03a6e1540cd
Amit <amit.s.pundir123@gmail.com>
2026-06-14 15:14:45 +0530

    feat: integrate @tanstack/react-query and add QueryProvider component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,12 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Public_Sans, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/components/ui/lib/utils";
+import { QueryProvider } from "@/components/providrs/query-provider";
 import { ThemeProvider } from "@/components/providrs/theme-provider";
 
-const interHeading = Inter({subsets:['latin'],variable:'--font-heading'});
+const interHeading = Inter({ subsets: ['latin'], variable: '--font-heading' });
 
-const publicSans = Public_Sans({subsets:['latin'],variable:'--font-sans'});
+const publicSans = Public_Sans({ subsets: ['latin'], variable: '--font-sans' });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,15 +35,17 @@ export default function RootLayout({
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", publicSans.variable, interHeading.variable)}
       suppressHydrationWarning
     >
-       <body className="min-h-full flex flex-col">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+      <body className="min-h-full flex flex-col">
+        <QueryProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="dark"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+            </ThemeProvider>
+        </QueryProvider>
       </body>
     </html>
   );
