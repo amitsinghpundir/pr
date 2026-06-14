917c64dc2b306c60721fb8d995854af94019891e
Amit <amit.s.pundir123@gmail.com>
2026-06-14 14:54:01 +0530

    feat: add new UI components including Skeleton, Slider, Toaster, Spinner, Switch, Table, Tabs, Textarea, Toggle Group, Toggle, and Tooltip
    - Introduced Skeleton component for loading placeholders.
    - Added Slider component for range selection.
    - Implemented Toaster component for notifications with customizable icons.
    - Created Spinner component for loading indicators.
    - Developed Switch component for toggling states.
    - Built Table component with subcomponents for structured data display.
    - Added Tabs component for organizing content into sections.
    - Implemented Textarea component for multi-line text input.
    - Created Toggle Group and Toggle components for grouped toggle buttons.
    - Developed Tooltip component for contextual hints and information.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Public_Sans, Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/components/ui/lib/utils";
+import { ThemeProvider } from "@/components/providrs/theme-provider";
+
+const interHeading = Inter({subsets:['latin'],variable:'--font-heading'});
+
+const publicSans = Public_Sans({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +31,19 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", publicSans.variable, interHeading.variable)}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+       <body className="min-h-full flex flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
